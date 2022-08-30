89a761243d8ac742c69712fe796a42ae729bb97f
ShulgaRuslan <shulgar1988@gmail.com>
2022-08-30 15:07:41 +0300

    final

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,39 @@
+
+const bodyEl = document.querySelector("body");
+const buttonStart = document.querySelector("button[data-start]");
+const buttonStop = document.querySelector("button[data-stop]");
+let timerId = null;
+let firstRun = true;
+
+buttonStop.disabled = true;
+
+buttonStart.addEventListener('click', runColor);
+buttonStop.addEventListener('click', stopColor);
+
+function runColor(){
+    buttonStart.disabled = true;
+    buttonStop.disabled = false;
+    timerId = setInterval(colorChange, 1000);
+    //цей код дає можливість побачити пешу зміну кольору одразу після натискання кнопки
+    if (firstRun){
+        colorChange();
+        firstRun = false;
+    };
+};
+
+function stopColor(){
+    buttonStart.disabled = false;
+    buttonStop.disabled = true;
+    clearInterval(timerId);
+    firstRun = true;
+};
+
+//Функція, яка змінює колір фону для <body>
+function colorChange(){
+    bodyEl.style.backgroundColor = getRandomHexColor();
+};
+
+//Функціця, яка генерує випадковий колір
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
